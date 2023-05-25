df531099785ddf643819335c640bcd6f2a8b3b3f
Dominikkkk <s21072@pjwstk.edu.pl>
2023-05-25 14:23:18 +0200

    First 2 parts of assigment

---
+++ BMI_create.sql
@@ -0,0 +1,39 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2023-05-24 11:45:24.858
+
+-- tables
+-- Table: BigMacIndex
+CREATE TABLE BigMacIndex (
+    Geografia_ID_Kraj integer NOT NULL,
+    Czas_Data date NOT NULL,
+    Local_Price double precision NOT NULL,
+    Dollar_Price double precision NOT NULL,
+    Dolar_PPP double precision NOT NULL,
+    CONSTRAINT BigMacIndex_Geografia FOREIGN KEY (Geografia_ID_Kraj)
+    REFERENCES Geografia (ID_Kraj),
+    CONSTRAINT BigMacIndex_Czas FOREIGN KEY (Czas_Data)
+    REFERENCES Czas (Data)
+);
+
+-- Table: Czas
+CREATE TABLE Czas (
+    Data date NOT NULL CONSTRAINT Czas_pk PRIMARY KEY,
+    Kwartal integer NOT NULL,
+    Rok_finansowy integer NOT NULL
+);
+
+-- Table: Geografia
+CREATE TABLE Geografia (
+    ID_Kraj integer NOT NULL CONSTRAINT Geografia_pk PRIMARY KEY,
+    Kraj varchar(64) NOT NULL,
+    Region varchar(6) NOT NULL,
+    Start_Data date NOT NULL,
+    End_Data date,
+    CONSTRAINT Ref_Start_Date FOREIGN KEY (Start_Data)
+    REFERENCES Czas (Data),
+    CONSTRAINT Ref_End_Date FOREIGN KEY (End_Data)
+    REFERENCES Czas (Data)
+);
+
+-- End of file.
+
